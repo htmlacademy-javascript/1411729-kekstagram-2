8246441f4626fbc9ea38389961b40e3a7b35c592
Aleksandr Reva <reva-alexander@yandex.ru>
2024-07-10 10:17:00 +0300

    создаёт функцию checksForPalindrome
    создаёт функцию для проверки, является ли строка палиндромом

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,17 @@
 const checksLengthString = (string, lengthMax) => string.length <= lengthMax;
+
+const checksForPalindrome = function (string) {
+  let invertedString = '', initialString;
+
+  if (string) {
+    initialString = String(string).toLowerCase().replaceAll(' ', '');
+
+    for (let index = initialString.length - 1; index >= 0; index--) {
+      invertedString += initialString[index];
+    }
+  } else {
+    initialString = false;
+  }
+
+  return initialString === invertedString;
+};
