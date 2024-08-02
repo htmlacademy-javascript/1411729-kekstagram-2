a59acda93ed4511311e1947707b59f155c7e82da
Aleksandr Reva <reva-alexander@yandex.ru>
2024-08-02 14:09:37 +0300

    добавляет сторонние функции
    добавляет функцию для генерации случайного числа и для получения случайного элемента из массива

--- js/main.js
+++ js/main.js
@@ -80,3 +80,14 @@ const NAMES = [
   'Женька',
   'Шинкарёв'
 ];
+
+// Функция для генерирования случайного числа в заданном диапазоне из проекта "Код и Магия"
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция для получения случайного элемента из проекта "Код и Магия"
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
