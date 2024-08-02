b1544aa1ea07c68a7f1cfa76f8c97b2c220267e4
Aleksandr Reva <reva-alexander@yandex.ru>
2024-08-02 14:14:46 +0300

    создает необходимые функции
    создает функцию для получения случайного комментария (одно или два предложения); функции для создания объектов комментария и фотографии и функцию для создания массива из сгенерированных функциями выше данных.

--- js/main.js
+++ js/main.js
@@ -91,3 +91,61 @@ const getRandomInteger = (a, b) => {
 
 // Функция для получения случайного элемента из проекта "Код и Магия"
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+// Функция вызвращает случайный комментарий содержащий один или два сообщения. Количество сообщений определяется случайным образом
+const getRandomMessage = function () {
+  if (getRandomInteger(1, 2) === 2) {
+    const firstMessage = getRandomArrayElement(MESSAGES);
+    let secondMessage = getRandomArrayElement(MESSAGES);
+
+    while (firstMessage === secondMessage) {
+      secondMessage = getRandomArrayElement(MESSAGES);
+    }
+
+    return `${firstMessage} ${secondMessage}`;
+  }
+
+  return getRandomArrayElement(MESSAGES);
+};
+
+const createComment = () => {
+  let idComment = 0;
+
+  return {
+    id: idComment++,
+    avatar: getRandomArrayElement(AVATARS_URL),
+    message: getRandomMessage(),
+    name: getRandomArrayElement(NAMES)
+  };
+};
+
+const createPhoto = () => {
+  const idPhoto = getRandomInteger(1, 25);
+
+  return {
+    id: idPhoto,
+    url: PHOTOS_URL[idPhoto - 1],
+    description: PHOTOS_DESCRIPTION[idPhoto - 1],
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({ length: getRandomInteger(0, 30) }, createComment)
+  };
+};
+
+const postPhotos = function (publishedPhotos = [], numberGeneratedPhotos = 25) {
+
+  while (publishedPhotos.length < numberGeneratedPhotos) {
+    const singlePhoto = createPhoto();
+
+    if (!publishedPhotos.some((publishedPhoto) => publishedPhoto.id === singlePhoto.id)) {
+      publishedPhotos.push(singlePhoto);
+    }
+
+    if (publishedPhotos.length === PHOTOS_URL.length) {
+      break;
+    }
+  }
+
+  return publishedPhotos;
+};
+
+console.table(postPhotos());
