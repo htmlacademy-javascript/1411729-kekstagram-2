2041ef4269ab1b9e284f8279b9c177862191df91
Aleksandr Reva <reva-alexander@yandex.ru>
2024-08-04 16:09:59 +0300

    создаёт и подключает модуль create-photo
    создаёт и подключает модуль для создания фотографии

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
-import {PHOTOS_URL, PHOTOS_DESCRIPTION} from './data.js';
-import {getRandomInteger} from './randomizers.js';
-import {createComment} from './create-comment.js';
-
-const createPhoto = () => {
-  const idPhoto = getRandomInteger(1, 25);
-
-  return {
-    id: idPhoto,
-    url: PHOTOS_URL[idPhoto - 1],
-    description: PHOTOS_DESCRIPTION[idPhoto - 1],
-    likes: getRandomInteger(15, 200),
-    comments: Array.from({ length: getRandomInteger(0, 30) }, createComment)
-  };
-};
+import {PHOTOS_URL} from './data.js';
+import {createPhoto} from './create-photo.js';
 
 const postPhotos = function (publishedPhotos = [], numberGeneratedPhotos = 25) {
 
