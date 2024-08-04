665670f79112dbc39effc98a3472b647915be8ff
Aleksandr Reva <reva-alexander@yandex.ru>
2024-08-04 15:29:07 +0300

    создаёт и подключает модуль randomizers
    создаёт и подключает модуль для функции получения случайного числа и функции получения случайного элемента

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
 import {PHOTOS_URL, PHOTOS_DESCRIPTION, AVATARS_URL, MESSAGES, NAMES} from './data.js';
-
-// Функция для генерирования случайного числа в заданном диапазоне из проекта "Код и Магия"
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-// Функция для получения случайного элемента из проекта "Код и Магия"
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+import {getRandomInteger, getRandomArrayElement} from './randomizers.js';
 
 // Функция вызвращает случайный комментарий содержащий один или два сообщения. Количество сообщений определяется случайным образом
 const getRandomMessage = function () {
