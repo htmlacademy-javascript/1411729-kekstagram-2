ac118ffad09e9208ca3dc9eb2a16b085b2336d4b
Aleksandr Reva <reva-alexander@yandex.ru>
2024-10-08 22:43:56 +0300

    реализует закрытие формы
    закрывает форму по клику на кнопку с крестиком и нажатию клавиши <Esc>, при этом значения всех полей формы сбрасываются

--- js/photo-upload-form.js
+++ js/photo-upload-form.js
@@ -1,20 +1,34 @@
+import {isEscapeKey, setupFormForSubmit} from './utils.js';
 import {pageContent} from './photo-modal';
 
 const imageUploadElement = document.querySelector('.img-upload__form');
 const imageEditorModal = imageUploadElement.querySelector('.img-upload__overlay');
-const imageCloseButton = imageUploadElement.querySelector('button[type="reset"]');
+const imageEditorCloseButton = imageUploadElement.querySelector('button[type="reset"]');
 
-const setupFormForSubmit = (form) => {
-  form.method = 'POST';
-  form.enctype = 'multipart/form-data';
-  form.action = ' https://31.javascript.htmlacademy.pro/kekstagram';
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closePhotoModal();
+  }
 };
 
 setupFormForSubmit(imageUploadElement);
 
-imageUploadElement.addEventListener('change', () => {
+function openPhotoModal() {
   pageContent.classList.add('modal-open');
   imageEditorModal.classList.remove('hidden');
 
-  // document.addEventListener('keydown', onDocumentKeydown);
-});
+  document.addEventListener('keydown', onDocumentKeydown);
+}
+
+function closePhotoModal() {
+  pageContent.classList.remove('modal-open');
+  imageEditorModal.classList.add('hidden');
+  imageUploadElement.reset();
+
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+imageUploadElement.addEventListener('change', openPhotoModal);
+
+imageEditorCloseButton.addEventListener('click', closePhotoModal);
