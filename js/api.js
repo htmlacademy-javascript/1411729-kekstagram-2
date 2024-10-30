e71d858d9841da9db7fee14b8189cc24350b648f
Aleksandr Reva <reva-alexander@yandex.ru>
2024-10-30 22:14:09 +0300

    добавляет обработку ошибок при загрузке

--- js/api.js
+++ js/api.js
@@ -1,5 +1,23 @@
+const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagram/data';
+const Route = {
+  GET_DATA: '/',
+  SEND_DATA: '/',
+};
+const ErrorText = {
+  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
+};
+
 const getData = () => fetch(
-  'https://31.javascript.htmlacademy.pro/kekstagram/data')
-  .then((response) => response.json());
+  `${BASE_URL}${Route.GET_DATA}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  })
+  .catch(() => {
+    throw new Error(ErrorText.GET_DATA);
+  });
 
 export {getData};
