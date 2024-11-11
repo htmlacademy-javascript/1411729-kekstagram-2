2c1a4944fd3b27228ee5c29324aa28de2c871cc9
Aleksandr Reva <reva-alexander@yandex.ru>
2024-11-11 20:59:32 +0300

    добавляет обработчик панели фильтров
    делает стилизацию кнопок фильтров изменяемой при клике по соответствующему

--- js/thumbnails-filters.js
+++ js/thumbnails-filters.js
@@ -1,3 +1,15 @@
 const filtersBar = document.querySelector('.img-filters');
+const filtersButtons = filtersBar.querySelector('.img-filters__form');
+
+filtersButtons.addEventListener('click', (evt) => {
+
+  Array.from(filtersButtons).forEach((button) => {
+    button.classList.remove('img-filters__button--active');
+
+    if (button.id === evt.target.id) {
+      button.classList.add('img-filters__button--active');
+    }
+  });
+});
 
 export {filtersBar};
